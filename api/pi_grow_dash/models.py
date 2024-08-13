a7c44c3a2990ff88ef31a9af5a2e88ef67560eea
Joe Bullard <joekbullard@gmail.com>
2024-08-13 22:55:06 +0100

    initial commit

---
+++ api/pi_grow_dash/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# {
+#   "nickname": "weather-test", 
+#   "model": "grow",
+#   "uid": "e6614c775b8c4035", 
+#   "timestamp": "2022-09-04T10:40:24Z", 
+#   "readings": {
+#     "temperature": 27.57,   // will change depending on board model
+#     "humidity": 49.33, 
+#     "pressure": 996.22, 
+#     "light": 0.41, 
+#     "moisture_1": 0.0, 
+#     "moisture_2": 0.0, 
+#     "moisture_3": 0.0, 
+#     "voltage": 4.954
+#   }
+# }
+
+class GrowReading(models.Model):
+    nickname = models.CharField(max_length=40)
+    uid = models.CharField(max_length=16)
+    timestmap = models.DateTimeField()
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    pressure = models.FloatField()
+    light = models.FloatField()
+    moisture_1 = models.FloatField()
+    moisture_2 = models.FloatField()
+    moisture_2 = models.FloatField()
+    voltage = models.FloatField()
